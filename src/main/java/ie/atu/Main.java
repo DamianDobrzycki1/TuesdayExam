42b6c29444114c5d76baabe39da3d638092cfe74
Damian <G00419511@atu.ie>
2023-12-12 14:02:05 +0000

    Added code from Ferry class and added new int

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -6,14 +6,20 @@ public class Main {
     public static void main(String[] args){
 
         int[] crew;
+        int[] passengers;
 
         crew = new int[2];
+        passengers = new int[2];
 
-        crew[0] = 10;
         crew[1] = 40;
 
+        passengers[1] = 50;
+
         for(int i = 1; i < crew.length; i++){
-            System.out.println("Minimum number of crew members is: " + crew[1]);
+            System.out.println("Minimum number of crew members on Ship is: " + crew[1]);
+        }
+        for(int i = 1; i <passengers.length; i++){
+            System.out.println("Maximum number of passengers on Ferry is: " + passengers[1]);
         }
 
         Ship myShip = new Ship("Titanic", "1985", "32");
