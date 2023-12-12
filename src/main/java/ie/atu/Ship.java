c7c3ea1199188da39a5c3ad6b6bda536aaa331df
Damian <G00419511@atu.ie>
2023-12-12 13:19:14 +0000

    Added crew int into Ship class

--- src/main/java/ie/atu/Ship.java
+++ src/main/java/ie/atu/Ship.java
@@ -1,4 +1,21 @@
 package ie.atu;
 
+import java.util.Scanner;
 public class Ship {
+    public static void main(String[] args){
+
+        String name;
+
+        String year;
+
+        int[] crew;
+
+        crew = new int[2];
+
+        crew[1] = 20;
+
+        for(int i = 1; i < crew.length; i++){
+            System.out.println("Select the amount of crew members that can enter: " + crew[1]);
+        }
+    }
 }
